af52c19f8b27bcf009e00a0c4907d96dddb1f92d
Everton Marques <everton.marques@gmail.com>
2016-04-06 10:09:10 -0300

    HTTP test.

--- dev/model_http_test.go
+++ dev/model_http_test.go
@@ -4,12 +4,15 @@ import (
 	//"fmt"
 	"io"
 	"net"
-	"strings"
+	//"strings"
+	"net/http"
 	"testing"
 	"time"
 )
 
 func TestHTTP1(t *testing.T) {
+
+	t.Logf("TestHTTP1: starting")
 
 	// launch bogus test server
 	addr := ":2001"
@@ -36,108 +39,24 @@ func TestHTTP1(t *testing.T) {
 
 func spawnServerHTTP(t *testing.T, addr string) (*testServer, error) {
 
+	t.Logf("spawnServerHTTP: will listen on %s", addr)
+
+	http.HandleFunc("/", rootHandler) // default handler
+
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
 
+	t.Logf("spawnServerHTTP: listening on %s", addr)
+
 	s := &testServer{listener: ln, done: make(chan int)}
 
-	go acceptLoopHTTP(t, s, handleConnectionHTTP)
+	go http.Serve(ln, nil)
 
 	return s, nil
 }
 
-func acceptLoopHTTP(t *testing.T, s *testServer, handler func(*testing.T, net.Conn)) {
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			t.Logf("acceptLoopHTTP: accept failure, exiting: %v", err)
-			break
-		}
-		go handler(t, conn)
-	}
-
-	close(s.done)
-}
-
-func handleConnectionHTTP(t *testing.T, c net.Conn) {
-	defer c.Close()
-
-	buf := make([]byte, 1000)
-
-	// send username prompt
-	if _, err := c.Write([]byte("hostname (ttyp0)\n\nlogin: ")); err != nil {
-		t.Logf("handleConnectionHTTP: send username prompt error: %v", err)
-		return
-	}
-
-	// consume username
-	if _, err := c.Read(buf); err != nil {
-		t.Logf("handleConnectionHTTP: read username error: %v", err)
-		return
-	}
-
-	// send password prompt
-	if _, err := c.Write([]byte("\nPassword: ")); err != nil {
-		t.Logf("handleConnectionHTTP: send password prompt error: %v", err)
-		return
-	}
-
-	// consume password
-	if _, err := c.Read(buf); err != nil {
-		t.Logf("handleConnectionHTTP: read password error: %v", err)
-		return
-	}
-
-	if _, err := c.Write([]byte("\n--- JUNOS 11.2R1.2 built 2011-06-22 02:55:58 UTC")); err != nil {
-		t.Logf("handleConnectionHTTP: send username prompt error: %v", err)
-		return
-	}
-
-LOOP:
-	for {
-		// send command prompt
-		if _, err := c.Write([]byte("\n{master:0}\nlab@host.domain> ")); err != nil {
-			t.Logf("handleConnectionHTTP: send command prompt error: %v", err)
-			return
-		}
-
-		// consume command
-		if _, err := c.Read(buf); err != nil {
-			if err == io.EOF {
-				return // peer closed connection
-			}
-			t.Logf("handleConnectionHTTP: read command error: %v", err)
-			return
-		}
-
-		str := string(buf)
-
-		switch {
-		case strings.HasPrefix(str, "q"): //quit
-			break LOOP
-		case strings.HasPrefix(str, "ex"): //exit
-			break LOOP
-		case strings.HasPrefix(str, "set cli"):
-		case strings.HasPrefix(str, "show conf"):
-			if _, err := c.Write([]byte("\nshow running-configuration")); err != nil {
-				t.Logf("handleConnectionHTTP: send sh run error: %v", err)
-				return
-			}
-		default:
-			if _, err := c.Write([]byte("\nIgnoring unknown command")); err != nil {
-				t.Logf("handleConnectionHTTP: send unknown command error: %v", err)
-				return
-			}
-		}
-
-	}
-
-	// send bye
-	if _, err := c.Write([]byte("\nbye\n")); err != nil {
-		t.Logf("handleConnectionHTTP: send bye error: %v", err)
-		return
-	}
-
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello web client\n")
 }
