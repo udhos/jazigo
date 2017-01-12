a55746830b1d9c684a6de16e8020238f72af881b
Everton Marques <everton.marques@gmail.com>
2017-01-12 18:47:31 -0200

    Mock filesystem for test.

---
+++ store/mock_filesystem.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"io"
+	"os"
+)
+
+// from https://talks.golang.org/2012/10things.slide#8
+
+var fs fileSystem = osFS{}
+
+type fileSystem interface {
+	Open(name string) (file, error)
+	Stat(name string) (os.FileInfo, error)
+}
+
+type file interface {
+	io.Closer
+	io.Reader
+	io.ReaderAt
+	io.Seeker
+	Stat() (os.FileInfo, error)
+}
+
+// osFS implements fileSystem using the local disk.
+type osFS struct{}
+
+func (osFS) Open(name string) (file, error)        { return os.Open(name) }
+func (osFS) Stat(name string) (os.FileInfo, error) { return os.Stat(name) }
